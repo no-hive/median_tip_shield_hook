13a43dc27648bb1d1316eafca9252b1ae174e187
no-hive <no-hive@users.noreply.github.com>
2026-07-30 17:58:45 +0000

    deploy: 54e6beeeb80703028aa0901e579e72d45af95675

--- script/00_DeployHook.s.sol
+++ script/00_DeployHook.s.sol
@@ -12,9 +12,7 @@ import {MedianPriorityFeeHook} from "../src/MPFHook.sol";
 contract DeployHookScript is BaseScript {
     function run() public {
         // hook contracts must have specific flags encoded in the address
-        uint160 flags = uint160(
-            Hooks.AFTER_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG
-        );
+        uint160 flags = uint160(Hooks.AFTER_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG);
 
         // Mine a salt that will produce a hook address with the correct flags
         bytes memory constructorArgs = abi.encode(poolManager);
