60c6a0c9c63e36debbf9e79f9a1a0d73427bcdfd
no-hive <no-hive@users.noreply.github.com>
2026-07-30 13:10:35 +0000

    deploy: 69360e92a526eeac9977fc2b16e6ac1b5077cee6

--- script/00_DeployHook.s.sol
+++ script/00_DeployHook.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.26;
 
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {HookMiner} from "@uniswap/v4-periphery/src/utils/HookMiner.sol";
-
+import {LPFeeLibrary} from "@uniswap/v4-core/src/libraries/LPFeeLibrary.sol";
 import {BaseScript} from "./base/BaseScript.sol";
 
 import {MedianPriorityFeeHook} from "../src/MPFHook.sol";
@@ -13,8 +13,7 @@ contract DeployHookScript is BaseScript {
     function run() public {
         // hook contracts must have specific flags encoded in the address
         uint160 flags = uint160(
-            Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
-                | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+            Hooks.AFTER_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG
         );
 
         // Mine a salt that will produce a hook address with the correct flags
