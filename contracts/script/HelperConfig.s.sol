e36ad76b741efcf31ec78ff7ad51b82c0b8ead7c
no-hive <no-hive@tutamail.com>
2026-08-04 17:30:33 +0300

    right solidity version in HelperConfig

--- contracts/script/HelperConfig.s.sol
+++ contracts/script/HelperConfig.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.26;
 import {Script} from "forge-std/Script.sol";
 
 // If we are on a local Anvil, we deploy the mocks
